5a281da8ba4bcf47b0bf9d59b0ab84be80357f43
Militalex <LeyAlexander5@web.de>
2025-09-21 21:27:57 +0200

    minor renaming

--- WirSchiffenDas-API-Library/src/main/java/com/github/militalex/module/ModulePort.java
+++ WirSchiffenDas-API-Library/src/main/java/com/github/militalex/module/ModulePort.java
@@ -40,7 +40,7 @@ public abstract class ModulePort {
     @KafkaListener(topicPartitions = @TopicPartition(topic = KafkaTopicConfig.FRONTEND_TOPIC,
             partitions = "" + KafkaTopicConfig.FrontendPartitions.PING),
             containerFactory = "pingKafkaListenerContainerFactory")
-    private void receivePing(@Header(KafkaHeaders.RECEIVED_KEY) int sessionId){
+    private void receivedPing(@Header(KafkaHeaders.RECEIVED_KEY) int sessionId){
         System.out.println("Received ping from session " + sessionId);
         module.getOptionalEquipments().forEach(optionalEquipment ->
                 sendAlgorithmState(sessionId, optionalEquipment, AlgorithmState.READY)
@@ -52,7 +52,7 @@ public abstract class ModulePort {
     @KafkaListener(topicPartitions = @TopicPartition(topic = KafkaTopicConfig.FRONTEND_TOPIC,
             partitions = "" + KafkaTopicConfig.FrontendPartitions.REQUEST_ANALYSIS),
             containerFactory = "receiveAnalysisRequestKafkaListenerContainerFactory")
-    private void receiveAnalysisRequestFromFrontend(@Header(KafkaHeaders.RECEIVED_KEY) int sessionId, @Payload AlgorithmResult emptyAlgoResult){
+    protected void receivedAnalysisRequestFromFrontend(@Header(KafkaHeaders.RECEIVED_KEY) int sessionId, @Payload AlgorithmResult emptyAlgoResult){
         OptionalEquipment optionalEquipment = emptyAlgoResult.getOptionalEquipment();
 
         if (module.getOptionalEquipments().contains(optionalEquipment)){
